30697b28183eaefc307202069b636aa6d5f6300d
jmilani224 <jmilani224@gmail.com>
2021-05-28 16:42:21 -0400

    removed tooltp

--- components/main.js
+++ components/main.js
@@ -44,7 +44,7 @@ const Main = ({ medArray }) => {
                 <BigText text="2. Review the Results" />
                 <LittleText text="Add or remove medications as needed, or visit the relevant LiverTox ebook page for more information." />
 
-                <ClipBoardIcon
+                <ClipBoardIconNoToolTip
                     w={10}
                     h={10}
                     p={2}
@@ -154,18 +154,18 @@ const ClipBoardIcon = ({ ...props }) => (
     </Tooltip>
 )
 
-function InstructionIcon({ }) {
-    return (
-        <Icon
-            w={12}
-            h={12}
-            p={2}
-            bgColor="brand.lightestBlue"
-            borderRadius="50%">
-
-        </Icon>
-    )
-}
+const ClipBoardIconNoToolTip = ({ ...props }) => (
+    <Icon
+        fill="none"
+        viewBox="0 0 128 128"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        {...props}
+    >
+        <path
+            d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+    </Icon>
+)
 
 function BigText({ text }) {
 
